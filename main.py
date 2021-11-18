611b89f5a6c73a710d306385a417e950e919873e
187154 <48299408+NathanNuckels@users.noreply.github.com>
2021-11-18 15:14:30 +0000

    Created README

--- main.py
+++ main.py
@@ -1,16 +1,1 @@
-'''
-A quick introduction.
-
-Recently, all my developer tools,
-Tor Browser, Cog, hell, even NOTEPAD
-are now blocked in ChromeOS.
-
-Mabee Ill make my ownnotepad mabee 
-ill jsut use ReplIt who knows. Since most of        
-my files are either Python or LaTeX, I set this repl to be in Python
-
-Not sure if this will make it to a git repo Since
-github is also blocked, but i will try my hardest.
-
-They killed notepad, now I must kill them.
-'''
+#oodoo python File
