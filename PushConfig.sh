8bdc56af0ced296ab4f6bdf721e8799071ce22bc
NathanNuckels <48299408+NathanNuckels@users.noreply.github.com>
2021-12-08 09:25:23 -0600

    Create PushConfig.sh

---
+++ PushConfig.sh
@@ -0,0 +1,10 @@
+#I use a lot of 'temperary' operating systems that reset their settings when you restart them
+#Stuff like virtuall macines that run in your web browser or Tails Linux
+
+
+git config --global user.name "NathanNuckels"
+git config --global user.email "nathannuckels@gmail.com"
+git config --global core.editor "namo -l "
+git config --global pull.ff false
+git config --global merge.ff false
+git config --global http.sslVerify false
